cbdaa97b3ce7f9c706f904594505e6a5b65af75a
mbuguajnr23 <mbuguaian32@gmail.com>
2024-10-24 18:15:48 +0200

    redesigned entire working of the application

--- app.py
+++ app.py
@@ -1,29 +1,64 @@
-from flask import Flask, request, render_template
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, render_template, redirect, url_for, session
+
 import requests
+
+#laod environment variables
+load_dotenv()
 
 app = Flask(__name__)
 
 # Spoonacular API details
-API_KEY = '3475738d335446f48a890c2b34da7299'
-API_URL = 'https://api.spoonacular.com/recipes/findByIngredients'
+api_key = os.getenv('API_KEY')
+# app.secret_key = 'TBA'
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
 
-@app.route('/fetch_recipes', methods=['POST'])
+    #get the list of ingredients from the session (or initialize it)
+
+    ingredients = session.get('ingredients'[])
+    return render_template('index.html', ingredients = ingredients)
+
+@app.route('/add_ingredient', methods=['POST'])
+def add_ingredient():
+    ingredient = request.form.get('Ingredient')
+
+    #add the new ingedient to the session
+    if 'ingedients' not in session:
+        session['ingredients'] = []
+
+    session['ingredients'].append(ingredient)
+
+    return redirect(url_for('index'))
+
+@app.route('/recipes')
+
 def fetch_recipes():
-    ingredients = request.form.get('ingredients')
-    params = {
-        'apiKey': API_KEY,
-        'ingredients': ingredients,
-        'number': 5  # Fetch 5 recipes
-    }
+    ingredients = session.get('ingredients',[])
     
-    response = requests.get(API_URL, params=params)
-    recipes = response.json()
+    if not ingredients:
+        return redirect(url_for('index')) #Redirect to index if no ingredients are available
     
-    return render_template('results.html', recipes=recipes)
+    #converts the ingredients to a comma-separated string for the API request
+    ingredients_str = ','.join(ingredients)
+
+    #fetch resipes from spoonacular API
+    response = requests.get(
+        f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients_str}&number=10&apiKey={API_KEY}'
+    )
+
+    recipe = response.json()
+
+
+    return render_template('recipe_detail.html', recipe=recipe)
+
+app.route('/clear_ingredints')
+def clear_ingredients():
+    #clear the ingredients from the session
+    session.pop('ingredients', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
